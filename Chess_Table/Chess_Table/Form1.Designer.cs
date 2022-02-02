21365c8bad184cfd2b914a34ba95ccf08d5ac6bb
Lehoczki Gergő <gerixy20@gmail.com>
2022-02-02 11:44:06 +0100

    Projekt hozzáadva

---
+++ Chess_Table/Chess_Table/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Chess_Table
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(33, 27);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(842, 597);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(953, 86);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(148, 157);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Draw Chess Table";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 653);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.canvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ Chess_Table/Chess_Table/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chess_Table
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        SolidBrush b;
+        SolidBrush b2;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+       
+       
+        public void DrawFromTop(float x, float y, float width, float heigth)
+        {
+            
+            bool nextLine = false;
+            y = canvas.Top;
+           
+                for (int i = 0; i < 4; i++)
+                {
+
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (nextLine)
+                        {
+                            if (j % 2 == 0)
+                            {
+                                b = new SolidBrush(Color.White);
+                            }
+                            else
+                            {
+                                b = new SolidBrush(Color.Black);
+                            }
+
+                        }
+                        if (!(nextLine))
+                        {
+                            if (!(j % 2 == 0))
+                            {
+                                b = new SolidBrush(Color.White);
+                            }
+                            else
+                            {
+                                b = new SolidBrush(Color.Black);
+                            }
+
+                        }
+
+                        g = canvas.CreateGraphics();
+                        x += 50;
+                        g.FillRectangle(b, x, y, width, heigth);
+                        Thread.Sleep(50);
+                       
+
+
+                    }
+                    if (nextLine)
+                    {
+                        nextLine = false;
+                    }
+                    else
+                    {
+                        nextLine = true;
+                    }
+
+                    y += width;
+                    x -= 8 * 50;
+                }
+
+            
+
+
+        }
+
+        public void DrawFromBottom(float x2,float y2,float width2,float heigth2)
+        {
+            bool nextLine = false;
+            y2 -= canvas.Top-8*50;
+            for (int i = 0; i < 4; i++)
+            {
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if (nextLine)
+                    {
+                        if (j % 2 == 0)
+                        {
+                            b2 = new SolidBrush(Color.Black);
+                        }
+                        else
+                        {
+                            b2 = new SolidBrush(Color.White);
+                        }
+
+                    }
+                    if (!(nextLine))
+                    {
+                        if (!(j % 2 == 0))
+                        {
+                            b2 = new SolidBrush(Color.Black);
+                        }
+                        else
+                        {
+                            b2 = new SolidBrush(Color.White);
+                        }
+
+                    }
+
+                    g = canvas.CreateGraphics();
+
+                    x2 += 50;
+                    g.FillRectangle(b2, x2, y2, width2, heigth2);
+                    Thread.Sleep(50);
+                  
+
+
+                }
+                if (nextLine)
+                {
+                    nextLine = false;
+                }
+                else
+                {
+                    nextLine = true;
+                }
+
+                y2 -= width2;
+                x2 -= 8 * 50;
+            }
+
+        }
+
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+            float x=0;
+            float y=0;
+            float width = 50;
+            float heigth = 50;
+            float x2 = 0;
+            float y2 = 0;
+            float width2 = 50;
+            float heigth2 = 50;
+            Thread t1 = new Thread(() => DrawFromTop(x, y, width, heigth));
+            Thread t2 = new Thread(() => DrawFromBottom(x2, y2, width2, heigth2));
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+           
+        }
+    }
+}
